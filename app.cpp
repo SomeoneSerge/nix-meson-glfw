b4e05ff489f3460a654555de08073e03293aab53
Someone Serge <sergei.kozlukov@aalto.fi>
2021-10-04 22:37:59 +0300

    draw a white triangle

--- app.cpp
+++ app.cpp
@@ -6,6 +6,28 @@
 #include <cassert>
 #include <iostream>
 
+const char *vtxSource = R"glsl(
+    #version 150 core
+
+    in vec2 position;
+
+    void main()
+    {
+        gl_Position = vec4(position, 0.0, 1.0);
+    }
+)glsl";
+
+const char *fragSource = R"glsl(
+   #version 150 core
+   
+   out vec4 outColor;
+
+   void main()
+   {
+    outColor = vec4(1.0, 1.0, 1.0, 1.0);
+   }
+)glsl";
+
 int main() {
   glfwInit();
 
@@ -22,12 +44,51 @@ int main() {
 
   glewExperimental = GL_TRUE;
   glewInit();
-
   assert(("GLEW initialization", glGenBuffers != nullptr));
-  GLuint vtxBuffer;
-  glGenBuffers(1, &vtxBuffer);
+
+  float vtxs[] = {0.0f, 0.5f, 0.5f, -0.5f, -0.5f, -0.5f};
+
+  GLuint vbo;
+  glGenBuffers(1, &vbo);
+  glBindBuffer(GL_ARRAY_BUFFER, vbo);
+  glBufferData(GL_ARRAY_BUFFER, sizeof(vtxs), vtxs, GL_STATIC_DRAW);
+
+  GLuint vtxShader = glCreateShader(GL_VERTEX_SHADER);
+  glShaderSource(vtxShader, 1, &vtxSource, nullptr);
+  glCompileShader(vtxShader);
+
+  GLuint fragShader = glCreateShader(GL_FRAGMENT_SHADER);
+  glShaderSource(fragShader, 1, &fragSource, nullptr);
+  glCompileShader(fragShader);
+
+  GLint vtxCompileStatus;
+  glGetShaderiv(vtxShader, GL_COMPILE_STATUS, &vtxCompileStatus);
+  assert(("Vertex shader compilation", vtxCompileStatus == GL_TRUE));
+
+  GLint fragCompileStatus;
+  glGetShaderiv(fragShader, GL_COMPILE_STATUS, &fragCompileStatus);
+  assert(("Fragment shader compilation", fragCompileStatus == GL_TRUE));
+
+  GLuint shaderProgram = glCreateProgram();
+  glAttachShader(shaderProgram, vtxShader);
+  glAttachShader(shaderProgram, fragShader);
+
+  glBindFragDataLocation(shaderProgram, 0, "outColor");
+
+  glLinkProgram(shaderProgram);
+  glUseProgram(shaderProgram);
+
+  GLuint vao;
+  glGenVertexArrays(1, &vao);
+  glBindVertexArray(vao);
+
+  GLint posAttrib = glGetAttribLocation(shaderProgram, "position");
+  glVertexAttribPointer(posAttrib, 2, GL_FLOAT, GL_FALSE, 0, 0);
+  glEnableVertexAttribArray(posAttrib);
 
   while (!glfwWindowShouldClose(window)) {
+    glDrawArrays(GL_TRIANGLES, 0, 3);
+
     glfwSwapBuffers(window);
     glfwPollEvents();
 
@@ -36,9 +97,13 @@ int main() {
     }
   }
 
-  std::cout << "Hello, meson!" << std::endl
-            << "Test vtxBuffer: " << vtxBuffer << std::endl;
+  std::cout << "Hello, white triangle!" << std::endl;
 
+  glDeleteProgram(shaderProgram);
+  glDeleteShader(vtxShader);
+  glDeleteShader(fragShader);
+  glDeleteBuffers(1, &vbo);
+  glDeleteVertexArrays(1, &vao);
   glfwTerminate();
   return 0;
 }

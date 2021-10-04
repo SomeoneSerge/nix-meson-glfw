40b3f0da47d65b6600ceb5312ed77c4847258905
Someone Serge <sergei.kozlukov@aalto.fi>
2021-10-04 21:18:19 +0300

    add empty glfw loop

--- app.cpp
+++ app.cpp
@@ -4,6 +4,25 @@
 int main() {
   glfwInit();
   std::cout << "Hello, meson!" << std::endl;
+
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
+  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+  glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+  glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
+
+  GLFWwindow *window = glfwCreateWindow(800, 600, "nix-meson-glfw", nullptr, nullptr);
+  glfwMakeContextCurrent(window);
+
+  while (!glfwWindowShouldClose(window)) {
+    glfwSwapBuffers(window);
+    glfwPollEvents();
+
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
+      glfwSetWindowShouldClose(window, GL_TRUE);
+    }
+  }
+
   glfwTerminate();
   return 0;
 }
